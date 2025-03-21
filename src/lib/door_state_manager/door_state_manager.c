5d2e150fce85f04e9521b9649e69d263873f702c
Agfa Prayoga Setiawan <agfa.ps@gmail.com>
2025-03-21 18:12:17 +0700

    nit: add TODO step commentary

--- src/lib/door_state_manager/door_state_manager.c
+++ src/lib/door_state_manager/door_state_manager.c
@@ -5,9 +5,26 @@
 void door_state_manager_init(void)
 {
     printf("door_state_manager_init\n");
+
+    // TODO LIST:
+    // init door state machine
+    // init ambient light sensor
+    // register callback to ambient light sensor
+    // init motion sensor
+    // register callback to motion sensor
+    // init distance sensor
+    // register callback to distance sensor
+    // determine initial door state
+    // set initial door state into door state machine
+    // optional, return initial state?
 }
 
 door_state_t door_state_manager_get_state(void)
 {
+    // TODO LIST:
+    // process event that trigger state change
+    // if door state is closed, what to do
+    // if door state is opened, what to do
+    // return current door state based on state machine
     return DOOR_CLOSED;
 }
