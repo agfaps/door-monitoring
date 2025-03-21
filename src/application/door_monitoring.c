f235aa4931c036b7484e15e03f01eb32585975ab
Agfa Prayoga Setiawan <agfa.ps@gmail.com>
2025-03-21 17:32:15 +0700

    feat(door-monitoring): add skeleton function

--- src/application/door_monitoring.c
+++ src/application/door_monitoring.c
@@ -11,6 +11,7 @@ void door_monitoring_init()
 
     // init door_state_manager
     printf("init door_state_manager\n");
+    // door_state_manager_init();
 }
 
 void door_monitoring_run()
@@ -19,9 +20,7 @@ void door_monitoring_run()
 
     // get initial door state from door_state_manager
     printf("get initial door state from door_state_manager\n");
-
-    // save initial door state in a variable
-    printf("save initial door state in a variable\n");
+    // current_state = door_state_manager_get_state();
 
     // entering forever loop
     printf("entering forever loop\n");
@@ -31,9 +30,15 @@ void door_monitoring_run()
         printf("after first starting up or wake up\n");
 
         // get door state from door_state_manager
+        // temp_state = door_state_manager_get_state();
+
         // compare with initial door state
-        // if different, print door state
-        printf("after doing door state check\n");
+        // if (temp_state != current_state)
+        // {
+            // if different, print door state
+        //     printf("DOOR STATE: %s\n", temp_state == OPENED ? "OPENED" : "CLOSED");
+        //     current_state = temp_state;
+        // }
 
         // goes to sleep
         printf("goes to sleep\n");
